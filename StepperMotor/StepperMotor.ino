12591eed5500a8f43255b849483cf15d9b9edda1
Vivian Li <vml39@cornell.edu>
2019-12-04 21:46:43 -0500

    run motors in loop

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -14,6 +14,9 @@ const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
 // number of steps required
 int steps_required = 100;
 
+// max speed
+const int speed_max = 700;
+
 //3D coordinate arrays - x,y,z
 int point3D[4];
 
@@ -35,17 +38,50 @@ AccelStepper stepper3(AccelStepper::FULL4WIRE, 37, 33, 35, 31);
 AccelStepper stepper4(AccelStepper::FULL4WIRE, 52, 48, 50, 46);
 AccelStepper stepper5(AccelStepper::FULL4WIRE, 44, 40, 42, 38);
 AccelStepper stepper6(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
-Stepper stepperMotor1 = Stepper(0, 53, 49, 51, 47);
-Stepper stepperMotor2 = Stepper(0, 45, 41, 43, 39);
-Stepper stepperMotor3 = Stepper(0, 37, 33, 35, 31);
-Stepper stepperMotor4 = Stepper(0, 52, 48, 50, 46);
-Stepper stepperMotor5 = Stepper(0, 44, 40, 42, 38);
-Stepper stepperMotor6 = Stepper(0, 36, 32, 34, 30);
+//Stepper stepperMotor1 = Stepper(0, 53, 49, 51, 47);
+//Stepper stepperMotor2 = Stepper(0, 45, 41, 43, 39);
+//Stepper stepperMotor3 = Stepper(0, 37, 33, 35, 31);
+//Stepper stepperMotor4 = Stepper(0, 52, 48, 50, 46);
+//Stepper stepperMotor5 = Stepper(0, 44, 40, 42, 38);
+//Stepper stepperMotor6 = Stepper(0, 36, 32, 34, 30);
 MultiStepper steppers;
+
+// Initialize stepper
+Stepper stepperMotor1(STEPS_PER_REV, 53, 49, 51, 47);
+Stepper stepperMotor2(STEPS_PER_REV, 45, 41, 43, 39);
+Stepper stepperMotor3(STEPS_PER_REV, 37, 33, 35, 31);
+Stepper stepperMotor4(STEPS_PER_REV, 52, 48, 50, 46);
+Stepper stepperMotor5(STEPS_PER_REV, 44, 40, 42, 38);
+Stepper stepperMotor6(STEPS_PER_REV, 36, 32, 34, 30);
+
+//turns motor to raise block
+void stepUp(Stepper stepperMotor) {
+  steps_required = STEPS_PER_OUT_REV / 2;
+  stepperMotor.setSpeed(1000);
+  stepperMotor.step(steps_required);
+}
+
+//turns motor to return block to position
+void stepDown(Stepper stepperMotor) {
+  steps_required = - STEPS_PER_OUT_REV / 2;
+  stepperMotor.setSpeed(1000);
+  stepperMotor.step(-steps_required);
+}
+
+//compare whether 2 arrays are equal
+boolean array_cmp(int *a, int *b){
+     int len_a = sizeof(a);
+     int len_b = sizeof(b);
+     if (len_a != len_b) return false;
+     for (int n=0;n<3;n++) if (a[n]!=b[n]) return false;
+
+     //ok, if we have not returned yet, they are equal :)
+     return true;
+}
 
 void setup() {
   Serial.begin(9600);
-  int speed_max = 1000;
+  Serial.println("starting...");
   stepperMotor1.setSpeed(speed_max);
   stepperMotor2.setSpeed(speed_max);
   stepperMotor3.setSpeed(speed_max);
@@ -60,6 +96,7 @@ void setup() {
   stepper6.setMaxSpeed(speed_max);
   steppers.addStepper(stepper1);
   steppers.addStepper(stepper2);
+//  steppers.addStepper(stepper3);
 //  steppers.addStepper(stepper4);
 //  steppers.addStepper(stepper5);
 //  steppers.addStepper(stepper6);
@@ -74,8 +111,15 @@ void loop() {
 //  positions[1] = 50;
 //  steppers.moveTo(positions);
 ////  stepper1.runSpeedToPosition();
-//  steppers.runSpeedToPosition(); // Blocks until all are in position
+//  steppers.runSpeedToPxosition(); // Blocks until all are in position
 //  delay(500);
+
+  stepperMotor1.step(4);
+  stepperMotor2.step(4);
+//  stepperMotor3.step(4);
+//  stepperMotor4.step(4);
+//  stepperMotor5.step(4);
+//  stepperMotor6.step(4);
 
   if (Serial.available() > 0) {
     int incoming = Serial.read();
@@ -147,41 +191,4 @@ void loop() {
 //    }
 //    Serial.println(incoming_char);
 //  }
-//
-//  // rotate CCW 1/2 turn slowly
-////  steps_required = -STEPS_PER_OUT_REV / 2;
-////  steppermotor.setSpeed(1000);
-////  steppermotor.step(steps_required);
-////  delay(1000);
-//
-//  // rotate CCW 1/2 turn quickly
-////  steps_required = - STEPS_PER_OUT_REV / 2;
-////  steppermotor.setSpeed (100);
-////  steppermotor.step(-steps_required);
-////  delay(2000);
-}
-
-//turns motor to raise block
-void stepUp(Stepper stepperMotor) {
-  steps_required = STEPS_PER_OUT_REV / 2;
-  stepperMotor.setSpeed(1000);
-  stepperMotor.step(steps_required);
-}
-
-//turns motor to return block to position
-void stepDown(Stepper stepperMotor) {
-  steps_required = - STEPS_PER_OUT_REV / 2;
-  stepperMotor.setSpeed(1000);
-  stepperMotor.step(-steps_required);
-}
-
-//compare whether 2 arrays are equal
-boolean array_cmp(int *a, int *b){
-     int len_a = sizeof(a);
-     int len_b = sizeof(b);
-     if (len_a != len_b) return false;
-     for (int n=0;n<3;n++) if (a[n]!=b[n]) return false;
-
-     //ok, if we have not returned yet, they are equal :)
-     return true;
 }
