8e9d7e5edc83735d6cfd82650d54048bb5137c75
Vivian Li <vml39@cornell.edu>
2019-12-11 16:43:04 -0500

    modify kinect and arduino code for all 30 motors

--- kinect/kinect.pde
+++ kinect/kinect.pde
@@ -2,15 +2,26 @@ import org.openkinect.processing.*;
 import processing.serial.*;
 
 Kinect kinect;
-Serial port;
+Serial portOne;
+Serial portTwo;
+Serial portThree;
 
-int pos = 1;
+int motor_pos = 0;
+int motors[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 int depth[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 //nums bw 0 and 2048
 void setup() {
   println(Serial.list());
-  port = new Serial(this, Serial.list()[1], 9600);
+  
+  // Set up the Serial ports
+  portOne = new Serial(this, Serial.list()[1], 9600);
+  portTwo = new Serial(this, Serial.list()[1], 9601);
+  portThree = new Serial(this, Serial.list()[1], 9602);
+  
+  portOne.bufferUntil(0x03);
+  portTwo.bufferUntil(0x03);
+  portThree.bufferUntil(0x03);
   
   // P3D for rendering something in 3D
   size(640, 480, P3D);
@@ -44,11 +55,23 @@ void draw() {
       // Map brightness value to a depth
       // Darker object in closer
       int z = (int) map(b, 0, 255, 9, 0);
-      port.write(depth[z]);
+      
+      if (motor_pos < 10) {
+        portOne.write(motors[motor_pos]);
+        portOne.write(depth[z]);
+      } else if (motor_pos < 20) {
+        portTwo.write(motors[motor_pos - 10]);
+        portTwo.write(depth[z]);
+      } else {
+        portThree.write(motors[motor_pos - 20]);
+        portThree.write(depth[z]);
+      }
       
       fill(b);
       
       rect(x, y, skip, skip);
+      
+      motor_pos += 1;
     }
   }
 }
--- test/test.ino
+++ test/test.ino
@@ -1,15 +1,44 @@
 #include <AccelStepper.h>
-// Define some steppers and the pins the will use
+
+// Define all of the steppers
 AccelStepper stepper1(AccelStepper::FULL4WIRE, 53, 49, 51, 47);
 AccelStepper stepper2(AccelStepper::FULL4WIRE, 45, 41, 43, 39);
 AccelStepper stepper3(AccelStepper::FULL4WIRE, 37, 33, 35, 31);
 AccelStepper stepper4(AccelStepper::FULL4WIRE, 52, 48, 50, 46);
 AccelStepper stepper5(AccelStepper::FULL4WIRE, 44, 40, 42, 38);
 AccelStepper stepper6(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper7(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper8(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper9(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper10(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper11(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper12(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper13(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper14(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper15(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper16(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper17(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper18(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper19(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper20(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper21(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper22(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper23(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper24(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper25(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper26(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper27(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper28(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper29(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
+AccelStepper stepper30(AccelStepper::FULL4WIRE, 36, 32, 34, 30);
 
-AccelStepper steppers[] = {stepper1, stepper2, stepper3, stepper4, stepper5, stepper6};
+AccelStepper steppersOne[] = {stepper1, stepper2, stepper3, stepper4, stepper5, stepper6, stepper7, stepper8, stepper9, stepper10};
+AccelStepper steppersTwo[] = {stepper11, stepper12, stepper13, stepper14, stepper15, stepper16, stepper17, stepper18, stepper19, stepper20};
+AccelStepper steppersThree[] = {stepper21, stepper22, stepper23, stepper24, stepper25, stepper26, stepper27, stepper28, stepper29, stepper30};
+
 
 const int LED = 12;
+const int MAX_SPEED = 500;
 const int MAX_EXTENSION = 1000;
 
 // Initialize all motor positions to be 0
@@ -20,12 +49,12 @@ int get_depth(byte c) {
     case '0':
       return 0;
     case '1':
-      return MAX_EXTENSION / 10;
+      return MAX_EXTENSION / 9;
     case '2':
       return 2 * MAX_EXTENSION / 9;
     case '3':
       return 3 * MAX_EXTENSION / 9;
-    case '4'
+    case '4':
       return 4 * MAX_EXTENSION / 9;
     case '5':
       return 5 * MAX_EXTENSION / 9;
@@ -42,33 +71,58 @@ int get_depth(byte c) {
   }
 }
 
+int get_stepper(byte motor) {
+  switch (motor) {
+    case 'a':
+      return 0;
+    case 'b':
+      return 1;
+    case 'c':
+      return 2;
+    case 'd':
+      return 3;
+    case 'e':
+      return 4;
+    case 'f':
+      return 5;
+    case 'g':
+      return 6;
+    case 'h':
+      return 7;
+    case 'i':
+      return 8;
+    case 'j':
+      return 9;
+  }
+}
+
+void moveMotor(AccelStepper steppers[], int motor, byte incoming) {
+  int pos = motor_pos[motor];
+  int depth = get_depth(incoming);
+  motor_pos[motor%10] = depth;
+
+  // Move the motor from the current position to the specified depth
+  int move_val = constrain(depth - pos, -MAX_EXTENSION, MAX_EXTENSION);
+  steppers[motor%10].moveTo(move_val); 
+  steppers[motor%10].run(); 
+}
+
 void setup()
 {
     pinMode(LED, OUTPUT);
-    int max_speed = 500;
     Serial.begin(9600);
-    for (int i = 0; i < 30; i++) {
-      steppers[i].setMaxSpeed(max_speed);
-      steppers[i].setAcceleration(100.0);
+    Serial1.begin(9601);
+    Serial2.begin(9602);
+    for (int i = 0; i < 10; i++) {
+      steppersOne[i].setMaxSpeed(MAX_SPEED);
+      steppersOne[i].setAcceleration(100.0);
+      steppersTwo[i].setMaxSpeed(MAX_SPEED);
+      steppersTwo[i].setAcceleration(100.0);
+      steppersThree[i].setMaxSpeed(MAX_SPEED);
+      steppersThree[i].setAcceleration(100.0);
     }
-    stepper1.setMaxSpeed(max_speed);
-    stepper1.setAcceleration(100.0);
-//    stepper3.setMaxSpeed(max_speed);
-//    stepper3.setAcceleration(100.0);
-//    stepper4.setMaxSpeed(max_speed);
-//    stepper4.setAcceleration(100.0);
-//    stepper5.setMaxSpeed(max_speed);
-//    stepper5.setAcceleration(100.0);
-//    stepper6.setMaxSpeed(max_speed);
-//    stepper6.setAcceleration(100.0);
-//    
-//    stepper2.setMaxSpeed(500.0);
-//    stepper2.setAcceleration(100.0);
-//    
-//    stepper3.setMaxSpeed(300.0);
-//    stepper3.setAcceleration(100.0);
-//    stepper3.moveTo(100000); 
 }
+
 void loop()
 {
     // Change direction at the limits
@@ -77,64 +131,29 @@ void loop()
 //    stepper1.run();
 //    stepper2.run();
 //    stepper3.run();
-  if (Serial.available() >= 30) {
-    for (int i = 0; i < 30; i++) {
-      byte incoming = Serial.read();
-      int pos = motor_pos[i];
-      int depth = get_depth(incoming);
-      motor_pos[i] = depth;
-      int move_val = constrain(depth - pos, -MAX_EXTENSION, MAX_EXTENSION);
-//    can this be negative?
-      steppers[i].moveTo(move_val); 
-      steppers[i].run(); 
-   }
 
-//    if (incoming_char == '1') {
-//      stepper1.moveTo(1000);
-//    }
-//    if (incoming_char == '2') {
-//      stepper2.moveTo(1000);
-//    }
-//    if (incoming_char == '3') {
-//      stepper3.moveTo(1000);
-//    }
-//    if (incoming_char == '4') {
-//      stepper4.moveTo(1000);
-//    }
-//    if (incoming_char == '5') {
-//      stepper5.moveTo(1000);
-//    }
-//    if (incoming_char == '6') {
-//      stepper6.moveTo(1000);
-//    }
+  if (Serial.available() >= 2) {
+    for (int i = 0; i < 30; i++) {
+      byte incoming_motor = Serial.read();
+      int motor = get_stepper(incoming_motor);
+      byte incoming_depth = Serial.read();
+      moveMotor(steppersOne, motor, incoming_depth);
+   }
   }
-  //moving adjacent blocks back to rest (position 0)
-//  if (stepper1.distanceToGo() < 999 & stepper1.distanceToGo() > 0) {
-//    stepper2.moveTo(0);
-//  }
-//  if (stepper2.distanceToGo() < 999 & stepper2.distanceToGo() > 0) {
-//    stepper1.moveTo(0);
-//    stepper3.moveTo(0);
-//  }
-//  if (stepper3.distanceToGo() < 999 & stepper3.distanceToGo() > 0) {
-//    stepper2.moveTo(0);
-//    stepper4.moveTo(0);
-//  }
-//  if (stepper4.distanceToGo() < 999 & stepper4.distanceToGo() > 0) {
-//    stepper5.moveTo(0);
-//    stepper6.moveTo(0);
-//  }
-//  if (stepper5.distanceToGo() < 999 & stepper5.distanceToGo() > 0) {
-//    stepper6.moveTo(0);
-//    stepper4.moveTo(0);
-//  }
-//  if (stepper6.distanceToGo() < 999 & stepper6.distanceToGo() > 0) {
-//    stepper5.moveTo(0);
-//  }
-//  stepper1.run();
-//  stepper2.run();
-//  stepper3.run();
-//  stepper4.run();
-//  stepper5.run();
-//  stepper6.run();
+  if (Serial1.available() >= 2) {
+    for (int i = 0; i < 30; i++) {
+      byte incoming_motor = Serial1.read();
+      int motor = get_stepper(incoming_motor) + 10;
+      byte incoming_depth = Serial1.read();
+      moveMotor(steppersTwo, motor, incoming_depth);
+   }
+  }
+  if (Serial2.available() >= 2) {
+    for (int i = 0; i < 30; i++) {
+      byte incoming_motor = Serial2.read();
+      int motor = get_stepper(incoming_motor) + 20;
+      byte incoming_depth = Serial2.read();
+      moveMotor(steppersThree, motor, incoming_depth);
+   }
+  }
 }
